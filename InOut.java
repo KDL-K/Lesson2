3f5f5c432fdff02b991e05c42d28f6a609a23514
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-25 21:40:26 +0300

    InOut
    Scanner

--- InOut.java
+++ InOut.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class Programm{
     public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("¬ведите число: ");
+        
+
+    }
 
 }
