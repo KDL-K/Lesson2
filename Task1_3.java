27315715cb5ff3c79c50a774e2ff998d4e819a2d
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-26 20:52:39 +0300

    Task1_3
    Making Task

---
+++ Task1_3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Task1_3{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Inpun your number: ");
+        int number=in.nextInt();
+        System.out.println("Your number is "+number);
+    }
+
+{
