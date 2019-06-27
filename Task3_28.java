7813970c053318e7ba66c77674305ff87cd18128
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 17:04:20 +0300

    Task3_28
    Finish

--- Task3_28.java
+++ Task3_28.java
@@ -1,5 +1,10 @@
 public class Task3_28{
     public static void main(String[] args){
-        
+        System.out.println("if(A>100 && B>100)");
+        System.out.println("if(A%2==0^B%2==0)");
+        System.out.println("if(A>0||B>0)");
+        System.out.println("if(A%3==0 && B%3==0 && C%3==0)");
+        System.out.println("if(A<50^B<50^C<50)");
+        System.out.println("if(A<0||B<0||C<0)");
     }
 }
