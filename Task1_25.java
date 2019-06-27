7521f164628a9dda2cd4e2385d934745edc260a4
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 17:11:19 +0300

    Task1_25
    Finish2

--- Task1_25.java
+++ Task1_25.java
@@ -5,5 +5,6 @@ public class Task1_25{
         System.out.print("Input size of the side of a square: ");
         int side=in.nextInt();
         System.out.println("The perimetre of the square: " +(side*4));
+        in.close();
     }
 }
