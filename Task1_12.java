f60fdc4a8fb67f25b0bea6745fdd539813eb9be7
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 11:01:29 +0300

    Task1_12
    Finish

--- Task1_12.java
+++ Task1_12.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Task1_12{
     public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Please input 4 integer numbers:");
+        System.out.print("t: ");
+        int t=in.nextInt();
+        System.out.print("v: ");
+        int v=in.nextInt();
+        System.out.print("x: ");
+        int x=in.nextInt();
+        System.out.print("y: ");
+        int y=in.nextInt();
+        System.out.println();
+        System.out.println("а) 5 10        б) 100 "+t+"        в) "+x+" 25");
+        System.out.println("   7 см           1949 "+v+"          "+x+" "+y);
         
     }
 }
