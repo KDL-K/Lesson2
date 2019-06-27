1ecb484e4cea85a7922dd59a31fa3caa63931d52
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 15:48:42 +0300

    Task1_60b
    Start&Finish

---
+++ Task1_60b.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Task1_60b{
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Input a: ");
+        int a=in.nextInt();
+        System.out.print("Input b: ");
+        int b=in.nextInt();
+        System.out.print("Input c: ");
+        int c=in.nextInt();
+        System.out.println("a="+a+"   b="+b+"   c="+c);
+        b=a;
+        c=b;
+        a=c;
+        System.out.println("After changes:");
+        System.out.println("a="+a+"   b="+b+"   c="+c);
+    }
+}
