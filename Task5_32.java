301813b57c49683cefb8c50595a210d36a845014
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 21:48:39 +0300

    Task5_32
    Finish

--- Task5_32.java
+++ Task5_32.java
@@ -1,5 +1,13 @@
 package com.company;
 import java.util.Scanner;
 public class Task5_32 {
-    Scanner in=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number n: ");
+        int n = in.nextInt();
+        double result = 1;
+        for (double i = 2; i <= n; i++) result += (1/i);
+        System.out.println("Result: " + result);
+        in.close();
+    }
 }
