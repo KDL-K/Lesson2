ab1d5f0285f9b02e7e7e4d2643589311f269109d
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 20:57:22 +0300

    Task5_3
    Start

---
+++ Task5_3.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Task5_3 {
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int menu;
+        do{
+            System.out.println("\tList of actions");
+            System.out.println("\tPress 1-5:");
+            System.out.println("1. Show all integer numbers (from 25 to 30) one under another");
+            if()
+        }while (menu!=5);
+
+
+    }
+}
