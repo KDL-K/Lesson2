be7b68779bff87ab0a4699044f24633c54a5cfbb
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-27 16:35:21 +0300

    Task3_6
    Finish

--- Task3_6.java
+++ Task3_6.java
@@ -1,5 +1,13 @@
 public class Task3_6{
     public static void main(String[] args){
-        
+        boolean X=false;
+        boolean Y=false;
+        boolean Z=true;
+        System.out.println("X||Y&&!Z="+(X||Y&&!Z));
+        System.out.println("!X&&!Y="+(!X&&!Y));
+        System.out.println("!(X&&Z)||Y="+(!(X&&Z)||Y));
+        System.out.println("X&&!Y||Z="+(X&&!Y||Z));   
+        System.out.println("X&&(!Y||Z)="+(X&&(!Y||Z)));  
+        System.out.println("X||(!(Y||Z))="+(X||(!(Y||Z))));
     }
 }
