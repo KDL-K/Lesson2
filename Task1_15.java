cae488a49c67e6bce8b58718e2fdc25851688104
KDL-K <52216074+KDL-K@users.noreply.github.com>
2019-06-28 09:04:08 +0300

    Task1_15
    Finish

--- Task1_15.java
+++ Task1_15.java
@@ -1,15 +1,15 @@
 public class Task1_15{
     public static void main(String[] args){
         System.out.println("à) -1/(x*x)");
-        System.out.println("á) a/b/c");
-        System.out.println("â) a*c/b");
+        System.out.println("á) a/(b*c)");
+        System.out.println("â) a/b*c");
         System.out.println("ã) (a+b)/2");
         System.out.println("ä) 5,45*(a+2b)/(2-a)");
-        System.out.println("å) ");
-        System.out.println("æ) (-b+1/a)*c/2");
-        System.out.println("ç) 1/(1+a/2+b/2)");
+        System.out.println("å) (-b+Math.sqrt(b*b-4*a*c))/(2*a)");
+        System.out.println("æ) (-b+1/a)/(2/c)");
+        System.out.println("ç) 1/(1+(a+b)/2)");
         System.out.println("è) 1/((1+(1/(2+(1/(2+3/5))))))");
-        System.out.println("ê)  ");
+        System.out.println("ê)  Math.pow(2,Math.pow(m,n))");
 
     }
 }
